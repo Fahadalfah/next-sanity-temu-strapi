30aa53cc7272812a73ecd06f93682918a0a97f22
Fahadalfah <aledwanifahad@gmail.com>
2025-09-02 07:11:54 +0300

    feat: add new features

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,25 @@
-import Image from "next/image";
+import { getCurrentSession } from "@/actions/auth";
+import SalesCampaignBanner from "@/components/layout/SalesCampaignBanner";
+import ProductGrid from "@/components/product/ProductGrid";
+import { getAllProducts } from "@/sanity/lib/client";
+import { urlFor } from "@/sanity/lib/image";
 
-export default function Home() {
+const Home = async () => {
+  const { user } = await getCurrentSession();
+  const products = await getAllProducts();
+  
+
   return (
-    <div>
-      FAHADDDDDDDDDDDDDD
-    </div>
+   <div>
+    <SalesCampaignBanner />
+    <section className=" container mx-auto px-4 py-8">
+     <ProductGrid products={products} /> 
+
+    </section>
+    
+    
+   </div>
   );
-}
+};
+
+export default Home;
